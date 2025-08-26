e60c57db44b1225df3b59ec7565bfeef7ea771e1
Sachin Sen <sachinsen1920@gmail.com>
2025-08-27 01:11:18 +0530

    add index file db

---
+++ api-marketplace/src/libs/db/models/index.ts
@@ -0,0 +1,6 @@
+export { User, type IUser } from "./User";
+export { API, type IAPI, type IAPIEndpoint } from "./API";
+export { APIKey, type IAPIKey } from "./APIKey";
+export { Subscription, type ISubscription } from "./Subscriptions";
+export { Usage, type IUsage } from "./Usage";
+export { Rating, type IRating } from "./Rating";
