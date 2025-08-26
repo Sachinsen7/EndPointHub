f4333c9341c0e456e3de0f6bcb26febbfd49874e
Sachin Sen <sachinsen1920@gmail.com>
2025-08-27 00:49:59 +0530

    add mongodb connection

---
+++ api-marketplace/src/libs/db/mongodb.ts
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const MONGO_URI = process.env.MONGODB_URI!;
+
+if (!MONGO_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  );
+}
+
+let cached = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null };
+}
+
+async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+      maxPoolSize: 10,
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000,
+      family: 4,
+    };
+
+    cached.promise = mongoose.connect(MONGO_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+    return cached.conn;
+  } catch (error) {
+    cached.promise = null;
+    throw error;
+  }
+
+  return cached.conn;
+}
+
+export default connectDB;
