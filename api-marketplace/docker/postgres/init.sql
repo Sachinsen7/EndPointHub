488655b6e84688a8ebd8d0f839a9ed0844a2e3d9
Sachin Sen <sachinsen1920@gmail.com>
2025-09-27 16:26:32 +0530

    add postgress init sql file

---
+++ api-marketplace/docker/postgres/init.sql
@@ -0,0 +1,12 @@
+-- Create extensions
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+-- Create updated_at function
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = CURRENT_TIMESTAMP;
+    RETURN NEW;
+END;
+$$ language 'plpgsql';
