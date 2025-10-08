8ea185d0994a3d8f1bacb09ab8a42444889b8941
Sachin Sen <sachinsen1920@gmail.com>
2025-10-08 14:04:08 +0530

    style(redis): standardize quotes and indentation in redis client

--- api-marketplace/src/libs/redis/client.ts
+++ api-marketplace/src/libs/redis/client.ts
@@ -1,26 +1,26 @@
-import Redis from "ioredis";
-import { config } from "../config/env";
-import { logger } from "../utils/logger";
+import Redis from 'ioredis';
+import { config } from '../config/env';
+import { logger } from '../utils/logger';
 
 let redis: Redis | null = null;
 
 export const getRedisCLient = () => {
-  if (!redis && config.redisUrl) {
-    redis = new Redis(config.redisUrl, {
-      enableReadyCheck: false,
-      maxRetriesPerRequest: null,
-    });
+    if (!redis && config.redisUrl) {
+        redis = new Redis(config.redisUrl, {
+            enableReadyCheck: false,
+            maxRetriesPerRequest: null,
+        });
 
-    redis.on("connect", () => {
-      logger.info("Redis client connected");
-    });
+        redis.on('connect', () => {
+            logger.info('Redis client connected');
+        });
 
-    redis.on("error", (error) => {
-      logger.error("Redis client error", error);
-    });
-  }
+        redis.on('error', (error) => {
+            logger.error('Redis client error', error);
+        });
+    }
 
-  return redis;
+    return redis;
 };
 
 export { redis };
